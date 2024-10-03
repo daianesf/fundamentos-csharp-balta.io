1fee56daf77e0dffc51a867d58d588db1e5ac52f
Daiane S Fernandes <156373089+daianesf@users.noreply.github.com>
2024-10-03 01:20:10 -0300

    Add files via upload

---
+++ ExemploTimeSpan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExemploTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.Clear();
+
+            var timeSpan = new TimeSpan();
+            Console.WriteLine(timeSpan);
+
+            var timeSpanNanosegundo = new TimeSpan(1);
+            Console.WriteLine(timeSpanNanosegundo);
+
+            var timeSpanHoraMinutoSegundo = new TimeSpan(5 , 12, 8);
+            Console.WriteLine(timeSpanHoraMinutoSegundo);
+
+            var timeSpanDiaHoraMinutoSegundo = new TimeSpan(3, 5, 50, 10);
+            Console.WriteLine(timeSpanDiaHoraMinutoSegundo);
+
+            var timeSpanNanosegundoMilissegundo = new TimeSpan();
+            Console.WriteLine(timeSpanNanosegundoMilissegundo);
+
+            Console.WriteLine(timeSpanHoraMinutoSegundo - timeSpanDiaHoraMinutoSegundo);
+            Console.WriteLine(timeSpanDiaHoraMinutoSegundo.Days);
+            Console.WriteLine(timeSpanDiaHoraMinutoSegundo.Add(new TimeSpan(12, 0, 0)));
+        }
+    }
+}
---
+++ HelloWorld/IfElse.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWord
+{
+    class IfElse
+    {
+        static void Main()
+        {
+            if (36 == 36)
+            {
+                Console.WriteLine("É igual");
+            }
+            Console.WriteLine("Finalizou o programa");
+        }
+    }
+}
